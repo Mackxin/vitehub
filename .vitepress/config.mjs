4d779af335871441d9e492e077ebbb44f920e123
mackxin <mackxin@aliyun.com>
2024-02-18 16:12:25 +0800

    update config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "bao",
+  base: '/vitehub/',
   description: "xiaobaobao",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
